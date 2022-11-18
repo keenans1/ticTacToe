0bb937b6a40b68bb8a4bf169bc6f4afb2200606e
Keenan Southall <keenan.southall@yahoo.com>
2022-11-17 17:47:33 -0700

    Remove player wins from game class

--- game.js
+++ game.js
@@ -2,8 +2,6 @@ class Game {
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.player1Wins = player1.wins;
-        this.player2Wins = player2.wins;
         this.player1Turn = true;
         this.player2Turn = false;
         this.draw = false;
